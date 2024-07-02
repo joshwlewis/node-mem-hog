70b05cf8b9cb8b0b666696b780382fe52334f262
Josh W Lewis <josh.w.lewis@gmail.com>
2024-07-02 14:00:35 -0500

    Read cgroups data

--- read-cgroups.sh
+++ read-cgroups.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-declare -a settings=(setting-in-bytes min low high max)
+echo "/proc/cgroups"
+cat /proc/cgroups
 
-echo "Reading cgroups memory settings"
+echo "/proc/self/cgroup"
+cat /proc/self/cgroup || true
+
+echo "/sys/fs/crgoup"
+stat -fc %T /sys/fs/cgroup/ || true
+ls -al /sys/fs/cgroup/ || true
+
+declare -a settings=(limit-in-bytes min low high max)
+echo "Reading cgroups settings from /sys/fs/cgroup/memory"
 for setting in "${settings[@]}"; do
     echo "Reading memory.${setting}"
-    cat "/sys/fs/cgroup/memory/memory.${setting}"
+    cat "/sys/fs/cgroup/memory/memory.${setting}" || true
 done
